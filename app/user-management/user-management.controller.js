ce13c74480615fdd21df8a5b7c2108476676478e
Nathan Rhijnsburger <nathan.rhijnsburger@gmail.com>
2018-01-25 03:25:34 +0100

    Moved unused controller

--- app/user-management/user-management.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// Declare app level module which depends on views, and components
-angular.module('userManagementApp')
-    .controller('UserManagementController', [ '$scope', function($scope) {
-        console.log('UserManagementController!');
-    }]);
